48fed5db2a1766fbddaa70796231dcda6c015f50
JoaoFelipe <joaofelipenp@gmail.com>
2013-10-26 22:52:00 -0200

    Initial commit: rails new + Gemfile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Subformbug::Application.config.secret_token = 'da0e4e5c410a2d7bd9fee52b83d544c8f5a63c491ed8cf74da0b46cc5119c0ee4cf38b8fb71818acda783ef2dd669e55856c061a3420f3a992ce8e4a0015e455'
